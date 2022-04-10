0e9bf60f42df7817d92658b0ef72fdbc9a42945e
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-10 13:22:54 -0400

    ternary operator

--- control_structures.php
+++ control_structures.php
@@ -3,15 +3,15 @@
 $my_array = [];
 
 //if else construct
-if (empty($my_array)) { 
-    echo "Array Empty!\n";
-} else {
-    echo "Array Not Empty!\n";
-}
+// if (empty($my_array)) { 
+//     echo "Array Empty!\n";
+// } else {
+//     echo "Array Not Empty!\n";
+// }
 
 //if else with comparison operators
 $name = "Sally";
-$age = 18;
+$age = 13;
 if ($age >= 18) {
     echo "{$name} is allowed to hold a TFSA account!\n";
 } else {
@@ -19,9 +19,22 @@ if ($age >= 18) {
 }
 
 //if else with comparison operators 2
-$contributionRoom = 6000;
+// $contributionRoom = 6000;
+// $contributionAmount = 0;
+// echo "{$name} is {$age} years old and her TFSA Contribution Room is CA\${$contributionRoom}!\n";
+// if ($age >= 18) {
+//     $contributionAmount = 500; 
+//     echo "{$name} made a contribution of CA\${$contributionAmount} towards her TFSA!\n";
+//     $contributionRoom = $contributionRoom - $contributionAmount;
+// } else {
+//     echo "{$name} is not allowed to hold a TFSA account!\n";
+// }
+// echo "{$name} is {$age} years old and now her TFSA Contribution Room is CA\${$contributionRoom}!\n";
+
+//if else with comparison operators 3
 $contributionAmount = 0;
-echo "{$name} is {$age} years old and her TFSA Contribution Room is CA\${$contributionRoom}!\n";
+$contributionRoom =  $age >= 18 ? 6000 : 0;
+echo "{$name} is {$age} years old and her TFSA Contribution Room is CA\${$contributionRoom}\n";
 if ($age >= 18) {
     $contributionAmount = 500; 
     echo "{$name} made a contribution of CA\${$contributionAmount} towards her TFSA!\n";
@@ -30,4 +43,3 @@ if ($age >= 18) {
     echo "{$name} is not allowed to hold a TFSA account!\n";
 }
 echo "{$name} is {$age} years old and now her TFSA Contribution Room is CA\${$contributionRoom}!\n";
-
