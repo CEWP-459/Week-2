2194b1609417d75c0fe738dc120a1be3be98272d
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-10 13:10:32 -0400

    if else with comparison operators

--- control_structures.php
+++ control_structures.php
@@ -1,11 +1,33 @@
 <?php
 
-$my_array = [77];
+$my_array = [];
 
+//if else construct
 if (empty($my_array)) { 
-    echo "Array Empty!";
+    echo "Array Empty!\n";
 } else {
-    echo "Array Not Empty!";
+    echo "Array Not Empty!\n";
 }
 
+//if else with comparison operators
+$name = "Sally";
+$age = 18;
+if ($age >= 18) {
+    echo "{$name} is allowed to hold a TFSA account!\n";
+} else {
+    echo "{$name} is not allowed to hold a TFSA account!\n";
+}
+
+//if else with comparison operators 2
+$contributionRoom = 6000;
+$contributionAmount = 0;
+echo "{$name} is {$age} years old and her TFSA Contribution Room is CA\${$contributionRoom}!\n";
+if ($age >= 18) {
+    $contributionAmount = 500; 
+    echo "{$name} made a contribution of CA\${$contributionAmount} towards her TFSA!\n";
+    $contributionRoom = $contributionRoom - $contributionAmount;
+} else {
+    echo "{$name} is not allowed to hold a TFSA account!\n";
+}
+echo "{$name} is {$age} years old and now her TFSA Contribution Room is CA\${$contributionRoom}!\n";
 
