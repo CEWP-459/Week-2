4ba9688540beb26770b3c254c861beddd7d051ac
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-10 13:35:33 -0400

    if else if else construct

--- control_structures.php
+++ control_structures.php
@@ -1,6 +1,6 @@
 <?php
 
-$my_array = [];
+// $my_array = [];
 
 //if else construct
 // if (empty($my_array)) { 
@@ -11,7 +11,7 @@ $my_array = [];
 
 //if else with comparison operators
 $name = "Sally";
-$age = 13;
+$age = 18;
 if ($age >= 18) {
     echo "{$name} is allowed to hold a TFSA account!\n";
 } else {
@@ -31,15 +31,26 @@ if ($age >= 18) {
 // }
 // echo "{$name} is {$age} years old and now her TFSA Contribution Room is CA\${$contributionRoom}!\n";
 
-//if else with comparison operators 3
+//if else with comparison operators 3 | Ternary Operator
 $contributionAmount = 0;
 $contributionRoom =  $age >= 18 ? 6000 : 0;
 echo "{$name} is {$age} years old and her TFSA Contribution Room is CA\${$contributionRoom}\n";
 if ($age >= 18) {
-    $contributionAmount = 500; 
+    $contributionAmount = 1200; 
     echo "{$name} made a contribution of CA\${$contributionAmount} towards her TFSA!\n";
     $contributionRoom = $contributionRoom - $contributionAmount;
 } else {
     echo "{$name} is not allowed to hold a TFSA account!\n";
 }
 echo "{$name} is {$age} years old and now her TFSA Contribution Room is CA\${$contributionRoom}!\n";
+
+//if else if else with comparison operators 4
+$membership;
+if ($contributionAmount < 500) {
+    $membership = "Silver";
+} else if ($contributionAmount >= 500 && $contributionAmount <= 1000) {
+    $membership = "Gold";
+} else {
+    $membership = "Platinum";
+}
+echo "{$name} is a {$membership} member of the TFSA policy!\n";
