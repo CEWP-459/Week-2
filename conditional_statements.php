fe0022ed7a0c819903d6326d4e806a36cef83a07
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-10 14:12:02 -0400

    switch case

--- conditional_statements.php
+++ conditional_statements.php
@@ -54,3 +54,31 @@ if ($contributionAmount < 500) {
     $membership = "Platinum";
 }
 echo "{$name} is a {$membership} member of the TFSA policy!\n";
+
+//switch operator
+$day = "Tuesday";
+switch ($day) {
+    case "Monday": 
+        echo "Monday";
+        break;
+    case "Tuesday":
+        echo "Tuesday";
+        break;
+    case "Wednesday":
+        echo "Wednesday";
+        break;
+    case "Thursday":
+        echo "Thursday";
+        break;
+    case "Friday":
+        echo "Friday";
+        break;
+    case "Saturday":
+        echo "Saturday";
+        break;
+    case "Sunday":
+        echo "Sunday";
+        break;
+    default: 
+        echo "Day Info Not Available!";   
+}
