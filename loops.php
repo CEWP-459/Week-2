fc807c6f44d734015815440f50fd835f4ce7f9c7
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-10 14:04:21 -0400

    do while loop

--- loops.php
+++ loops.php
@@ -2,11 +2,11 @@
 
 //while loop
 $month = 1;
-echo "WHILE LOOP\n";
-while ($month <= 12) {
-    echo $month . ",";
-    $month++;
-}
+//echo "WHILE LOOP\n";
+// while ($month <= 12) {
+//     echo $month . ",";
+//     $month++;
+// }
 
 //for loop
 echo "FOR LOOP\n";
@@ -14,7 +14,12 @@ for ($i = 1; $i <= 12; $i++) {
     echo $i . ",";
 }
 
-
+//do while loop
+echo "DO WHILE LOOP\n";
+do {
+    echo $month . ",";
+    $month++;
+} while ($month <= 12);
 
 
 
