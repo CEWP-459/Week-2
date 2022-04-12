3806798fbe3cdd6996c5f4bea92996217c3e9d85
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-11 21:31:26 -0400

    math functions

---
+++ built_in_functions.php
@@ -0,0 +1,29 @@
+<?php
+
+echo pow(2, 4);
+
+echo "<br>";
+
+echo rand(1, 1000); //rand()
+
+echo "<br>";
+
+echo sqrt(100);
+
+echo "<br>";
+
+echo ceil(98.76);
+
+echo "<br>";
+
+echo floor(98.76);
+
+echo "<br>";
+
+echo round(98.55);
+
+echo "<br>";
+
+echo round(98.49);
+
+echo "<br>";
