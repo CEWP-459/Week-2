89566a965188763995d3edfac5d287733daedd86
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-11 21:43:51 -0400

    string functions

--- built_in_functions.php
+++ built_in_functions.php
@@ -1,29 +1,30 @@
 <?php
 
+//Math Functions
 echo pow(2, 4);
-
 echo "<br>";
-
 echo rand(1, 1000); //rand()
-
 echo "<br>";
-
 echo sqrt(100);
-
 echo "<br>";
-
 echo ceil(98.76);
-
 echo "<br>";
-
 echo floor(98.76);
-
 echo "<br>";
-
 echo round(98.55);
-
+echo "<br>";
+echo round(98.49);
 echo "<br>";
 
-echo round(98.49);
-
+//String Functions
+$string = "Hi, how are you?";
+echo strlen($string);
+echo "<br>";
+// echo count($string); //wrong!!!
+echo "<br>";
+echo strtoupper($string);
+echo "<br>";
+echo $string;
+echo "<br>";
+echo strtolower($string);
 echo "<br>";
