8e03f7d16a83c732867b589c23e95d42f4584f54
Kritika Sharma <35176749+KritikaSharmaKS@users.noreply.github.com>
2022-04-11 21:17:41 -0400

    const keyword

--- constants.php
+++ constants.php
@@ -4,7 +4,10 @@ $x = 10;
 echo $x . "<br>";
 
 define("NAME", "Kritika Sharma"); //old way
-
 echo NAME . "<br>";
-echo constant("NAME") . "<br>"; 
-print_r(get_defined_constants());
+
+// echo constant("NAME") . "<br>"; 
+// print_r(get_defined_constants());
+
+const NEW_NAME = "KS";
+echo NEW_NAME . "<br>";
